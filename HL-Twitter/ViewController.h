9b5a1afffa15471a0d65fa39d31b46302dc31b50
Vicente Santacoloma <vsantacoloma@gmail.com>
2015-03-31 10:17:33 +0100

    Initial Commit

---
+++ HL-Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HL-Twitter
+//
+//  Created by Vicente Santacoloma on 31/03/2015.
+//  Copyright (c) 2015 Hargreaves Lansdown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HL-Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HL-Twitter
+//
+//  Created by Vicente Santacoloma on 31/03/2015.
+//  Copyright (c) 2015 Hargreaves Lansdown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
